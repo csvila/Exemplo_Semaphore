ef127a047a0319e71d9ceb57d7d8ad63c02c855a
csvila <cesarvilarim@CESARAUGUSTC8D7>
2019-07-31 11:53:08 -0300

    Exemplo Semáforo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExemploSemaphore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa Iniciado");
+            ExemploSemaforo.ThreadOrdenada();
+            ExemploSemaforo.ThreadAleatoria();
+            Console.WriteLine("Aguarde as threads serem executadas");
+            Console.ReadKey();
+        }
+    }
+}
